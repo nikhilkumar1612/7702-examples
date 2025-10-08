68f3a3f66b73c8f1b3a107e8687535f3956a2d93
nikhil kumar <nikhilkumarpps@gmail.com>
2025-10-08 17:20:50 +0530

    ft: adding kernel example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 7702-examples
-contains sample code snippets for different 7702 account implementations.
+contains sample code snippets for different 7702 account implementations. Using [etherspot's free bundler sdk](https://www.npmjs.com/package/@etherspot/free-bundler)
+
+### install dependencies
+```
+npm i
+```
+
+### send userop using [Kernel's 7702 implementation](https://github.com/zerodevapp/kernel/blob/dev/src/Kernel.sol)
+```
+npx tsx src/accounts/kernel.ts
+```
---
+++ src/accounts/kernel.ts
@@ -0,0 +1,53 @@
+import { createKernelAccount } from "@zerodev/sdk";
+import { KERNEL_V3_3, KernelVersionToAddressesMap } from "@zerodev/sdk/constants";
+import { privateKeyToAccount } from "viem/accounts"
+import dotenv from "dotenv";
+import { createPublicClient, Hex, http, parseUnits, publicActions, walletActions } from "viem";
+import { createFreeBundler } from "@etherspot/free-bundler";
+import { sepolia } from "viem/chains";
+import { entryPoint07Address } from "viem/account-abstraction";
+dotenv.config();
+
+const main = async () => {
+    const owner = privateKeyToAccount(process.env.PRIVATE_KEY! as Hex);
+
+    const bundlerUrl = process.env.BUNDLER_URL
+
+    const publicClient = createPublicClient({
+        transport: http(bundlerUrl),
+        chain: sepolia
+    });
+
+    const kernelAccount = await createKernelAccount(
+        publicClient,
+        {
+            entryPoint: { address: entryPoint07Address, version: "0.7" },
+            kernelVersion: KERNEL_V3_3,
+            eip7702Account: owner
+        },
+    );
+
+    const bundlerClient = createFreeBundler({
+        chain: sepolia,
+        bundlerUrl
+    }).extend(publicActions).extend(walletActions);
+
+    const authorization = await bundlerClient.signAuthorization({
+        account: owner,
+        contractAddress: KernelVersionToAddressesMap[KERNEL_V3_3].accountImplementationAddress,
+    });
+
+    console.log("authorization:: ", authorization);
+
+    const userOpHash = await bundlerClient.sendUserOperation({
+        account: kernelAccount,
+        authorization,
+        calls: [{
+            to: "0x03b22d7742fA2A8a8f01b64F40F0F2185E965cB8",
+            value: parseUnits('0.0001', 18)
+        }]
+    });
+
+    console.log("userop hash:: ", userOpHash);
+}
+main();
